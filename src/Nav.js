10248fdc5ab6c92df56bad5b9b573fda3078041c
Neil Krichi <neilkrichi@gmail.com>
2022-06-15 23:51:13 -0700

    adding home link to nav

--- src/Nav.js
+++ src/Nav.js
@@ -18,6 +18,7 @@ const Nav = () => {
             </div>
             <div className="navigation">
                 <ul>
+                    <li><Link to="/" >Home</Link></li>
                     <li><Link to="/about" >About</Link></li>
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
